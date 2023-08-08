1850325bbe785059555440c11b88759869666bb3
Evandro Oliveira de Novais <evandro-novais@outlook.com>
2023-08-08 15:33:47 -0300

    Created ProductController and DTO)

--- src/main/java/com/eontecnologia/springboot/repositories/ProductRepository.java
+++ src/main/java/com/eontecnologia/springboot/repositories/ProductRepository.java
@@ -2,8 +2,11 @@ package com.eontecnologia.springboot.repositories;
 
 import com.eontecnologia.springboot.models.ProductModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface ProductRepository extends JpaRepository<ProductModel, UUID> {
+
 }
